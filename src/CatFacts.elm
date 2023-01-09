7de106b27ee2ee9cb0d88cfe099c802688d04f6f
Austin <aengelbrecht>
2023-01-09 16:02:38 -0600

    ELM Boilerplate App

--- src/CatFacts.elm
+++ src/CatFacts.elm
@@ -1,1 +1,52 @@
 module CatFacts exposing (..)
+
+import Browser
+import Html
+
+
+-- MAIN
+
+main = 
+  Browser.sandbox 
+    { init = init
+    , update = update 
+    , view = view 
+    }
+
+
+-- MODEL
+type alias Model = Int
+
+
+init : Model 
+init = 
+  0 
+
+
+-- UPDATE
+
+type Msg
+  = Increment
+  | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
