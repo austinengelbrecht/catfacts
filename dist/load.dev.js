123028e04c17c27f9c1251d71d1f3acb56a1faa6
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-01-14 21:54:02 -0600

    Linting files - Changes to gitignore - Removed uncesscary compiled files

--- dist/load.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-document.addEventListener("DOMContentLoaded", function () {
-  var app = Elm.CatFacts.init({
-    node: document.getElementById("app")
-  });
-});
--- load.js
+++ load.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   let app = Elm.CatFacts.init({ node: document.getElementById("app") });
-})
+});
